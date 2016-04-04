2d59b6271e2570be890686371823ee0693f249c1
jmeyer11 <j_meyer11@me.com>
2016-04-04 13:28:36 -0400

    Train Controller Subsystem prototype

---
+++ TrainController/TrainController/TrainController 5.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainController
+{
+    class TrainController
+    {
+        private static TrainControllerUserInterface ui;
+        public int SetAuthority { get; set; }
+        public int SetSpeed { get; set; }
+        public int CurrentVelocity { get; set; }
+        public int OutsideTemperature { get; set; }
+        public Boolean DarkOutside { get; set; }
+        private static double outputPower = 0;
+
+         public TrainController(int trainID)
+         {
+            ui = new TrainControllerUserInterface(trainID);
+            ui.Show();
+         }
+
+        public double CalculatePower()
+        {
+            outputPower = SetAuthority;
+            return outputPower;
+        }
+
+        public Boolean GetLightStatus()
+        {
+            return DarkOutside;
+        }
+
+        public void SetPassengerEmergencyBrake()
+        {
+
+        }
+    }
+}
